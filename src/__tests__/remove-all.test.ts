4f9c1be5c67b2b02c0ef8bbabb84c455ebceed1d
Luca Tagliabue <luca.tagliabue@radicalbit.io>
2024-06-27 11:22:49 +0200

    feat: wrote test for the removeAll

--- src/__tests__/remove-all.test.ts
+++ src/__tests__/remove-all.test.ts
@@ -2,86 +2,37 @@ import { act, renderHook } from '@testing-library/react'
 import useFormbit from 'src/use-formbit'
 import * as Yup from 'yup'
 
-const yup = Yup.object()
+const schema = Yup.object({
+  firstName: Yup.string().required(),
+  lastName: Yup.string().required(),
+  age: Yup.number().min(18).required()
+})
 
-describe('remove fn', () => {
-  it('Should remove the given field from the form', () => {
-    const initialValues = {
-      name: 'Harry'
-    }
-
-    const { result, unmount } = renderHook(() => useFormbit({ initialValues, yup }))
-
-    act(() => result.current.remove('name'))
-
-    expect(result.current.form).toStrictEqual({})
-
-    unmount()
-  })
-
-  it('Should remove the given nested field from the form', () => {
-    const initialValues = {
-      user: { name: 'Harry', lastName: 'Potter' }
-    }
-
-    const { result, unmount } = renderHook(() => useFormbit({ initialValues, yup }))
-
-    act(() => result.current.remove('user.name'))
-
-    expect(result.current.form).toStrictEqual({ user: { lastName: 'Potter' } })
-
-    unmount()
-  })
-
-  it('Should set isDirty to true after writing', () => {
-    const initialValues = {
-      user: { name: 'Harry', lastName: 'Potter' }
-    }
-
-    const { result, unmount } = renderHook(() => useFormbit({ initialValues, yup }))
-
-    act(() => result.current.remove('user.name'))
-
-    expect(result.current.form).toStrictEqual({ user: { lastName: 'Potter' } })
-
-    expect(result.current.isDirty).toBe(true)
-
-    unmount()
-  })
-
-  it('Should execute given successCallback only once', () => {
-    const initialValues = {
-      name: 'Harry'
-    }
-    const successCallback = jest.fn()
-
-    const { result, unmount } = renderHook(() => useFormbit({ initialValues, yup }))
-
-    act(() => result.current.remove('name', { successCallback }))
-
-    expect(result.current.form).toStrictEqual({ })
-    expect(successCallback).toHaveBeenCalledTimes(1)
-
-    unmount()
-  })
-
-  it('Should execute given errorCallback only once', () => {
-    const initialValues = {
-      name: 'Harry',
-      age: 2
-    }
-
-    const schema = Yup.object({ age: Yup.number().min(18) })
-
-    const errorCallback = jest.fn()
+describe('removeAll fn', () => {
+  it('Should remove the given fields from the form', () => {
+    const initialValues = { firstName: 'Jane', lastName: 'Doe', age: 23 }
+    const expectedValues = { firstName: undefined, lastName: undefined, age: 23 }
 
     const { result, unmount } = renderHook(() => useFormbit({ initialValues, yup: schema }))
 
-    act(() => result.current.remove('name', { errorCallback, pathsToValidate: ['age'] }))
+    act(() => result.current.removeAll(['firstName', 'lastName']))
 
-    expect(result.current.form).toStrictEqual({ age: 2 })
+    expect(result.current.form).toStrictEqual(expectedValues)
 
-    expect(errorCallback).toHaveBeenCalledTimes(1)
+    unmount()
+  })
+
+  it('Should remove just the given fields from the form', () => {
+    const initialValues = { firstName: 'Jane', lastName: 'Doe', age: 17 }
+    const expectedValues = { firstName: undefined, lastName: undefined, age: 24 }
+
+    const { result, unmount } = renderHook(() => useFormbit({ initialValues, yup: schema }))
+
+    act(() => result.current.validateForm())
+    act(() => result.current.write('age', 24, { pathsToValidate: ['age'] }))
+    act(() => result.current.removeAll(['firstName', 'lastName']))
+
+    expect(result.current.form).toStrictEqual(expectedValues)
 
     unmount()
   })
